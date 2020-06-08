a5bdc821fc490221e2a38a2215e6ef0c9d37b4fc
Caitano <danielcaitano9@hotmail.com>
2020-06-08 17:53:16 -0300

    Atualizações para rodar no servidor online

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const sqlite3 = require('sqlite3')
 
 const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT /*Porta do servidor online*/ || 3000 
+const path = require('path')
 
-const dbConnection = new sqlite3.Database(('banco.sqlite'),
+const dbConnection = new sqlite3.Database(path.resolve(__dirname, 'banco.sqlite'),
     sqlite3.OPEN_READWRITE || sqlite3.OPEN_CREATE, (err) => {
         if (err) {
             console.log('erro: ' + err)
@@ -17,10 +18,11 @@ const dbConnection = new sqlite3.Database(('banco.sqlite'),
         }
     });
 
+app.set('views', path.join(__dirname, 'views'/*pasta views esse parametro */))    
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({ extended: true }))  
 
 app.get('/', async (request, response) => {
     const db = await dbConnection
